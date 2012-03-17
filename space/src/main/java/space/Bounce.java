ec0d15b7ba1f8b55503502fa80fe6943b500415d
Johannes Brodwall <johannes@brodwall.com>
2012-03-17 23:48:47 +0100

    Remove dead code

--- space/src/main/java/space/Bounce.java
+++ space/src/main/java/space/Bounce.java
@@ -21,35 +21,25 @@ public class Bounce extends Space {
             for (PhysicalObject other : objects) {
                 if (one == other || remove.contains(other))
                     continue;
-                if (!true) {
-                    if (Math.sqrt(Math.pow(one.x - other.x, 2) + Math.pow(one.y - other.y, 2)) < 5e9) {
-                        one.absorb(other);
-                        remove.add(other);
-                    }
-                } else {
-                    double distance = Math.sqrt(Math.pow(one.x - other.x, 2) + Math.pow(one.y - other.y, 2));
-                    double collsionDistance = one.radius + other.radius;
-                    if (distance < collsionDistance) {
-                        one.hitBy(other);
-                    }
+                double distance = Math.sqrt(Math.pow(one.x - other.x, 2) + Math.pow(one.y - other.y, 2));
+                double collsionDistance = one.radius + other.radius;
+                if (distance < collsionDistance) {
+                    one.hitBy(other);
                 }
             }
-            // Wall collision reverses speed in that direction
-            if (true) {
-                if (one.x - one.radius < 0) {
-                    one.vx = -one.vx;
-                }
-                if (one.x + one.radius > 800) {
-                    one.vx = -one.vx;
-                }
-                if (one.y - one.radius < 0) {
-                    one.vy = -one.vy;
-                }
-                if (one.y + one.radius > 800 && !IS_BREAKOUT) {
-                    one.vy = -one.vy;
-                } else if (one.y - one.radius > 800) {
-                    remove.add(one);
-                }
+            if (one.x - one.radius < 0) {
+                one.vx = -one.vx;
+            }
+            if (one.x + one.radius > 800) {
+                one.vx = -one.vx;
+            }
+            if (one.y - one.radius < 0) {
+                one.vy = -one.vy;
+            }
+            if (one.y + one.radius > 800 && !IS_BREAKOUT) {
+                one.vy = -one.vy;
+            } else if (one.y - one.radius > 800) {
+                remove.add(one);
             }
         }
         objects.removeAll(remove);
@@ -57,47 +47,35 @@ public class Bounce extends Space {
 
     @Override
     protected void doStep() {
-        if (!false) {
-            for (PhysicalObject aff : objects) {
-                double fx = 0;
-                double fy = 0;
-                for (PhysicalObject oth : objects) {
-                    if (aff == oth)
-                        continue;
-                    double[] d = new double[]{aff.x - oth.x, aff.y - oth.y};
-                    double r2 = Math.pow(d[0], 2) + Math.pow(d[1], 2);
-                    double f = G * aff.mass * oth.mass / r2;
-                    double sqrtOfR2 = Math.sqrt(r2);
-                    fx += f * d[0] / sqrtOfR2;
-                    fy += f * d[1] / sqrtOfR2;
-                }
-                double ax = fx / aff.mass;
-                double ay = fy / aff.mass;
-                aff.x = aff.x - ax * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vx * PhysicalObject.seconds;
-                aff.y = aff.y - ay * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vy * PhysicalObject.seconds;
-                aff.vx = aff.vx - ax * PhysicalObject.seconds;
-                aff.vy = aff.vy - ay * PhysicalObject.seconds;
+        for (PhysicalObject aff : objects) {
+            double fx = 0;
+            double fy = 0;
+            for (PhysicalObject oth : objects) {
+                if (aff == oth)
+                    continue;
+                double[] d = new double[]{aff.x - oth.x, aff.y - oth.y};
+                double r2 = Math.pow(d[0], 2) + Math.pow(d[1], 2);
+                double f = G * aff.mass * oth.mass / r2;
+                double sqrtOfR2 = Math.sqrt(r2);
+                fx += f * d[0] / sqrtOfR2;
+                fy += f * d[1] / sqrtOfR2;
             }
-        } else {
-            for (PhysicalObject physicalObject : objects) {
-                physicalObject.x = physicalObject.x + physicalObject.vx * PhysicalObject.seconds;
-                physicalObject.y = physicalObject.y + physicalObject.vy * PhysicalObject.seconds;
-            }
-
+            double ax = fx / aff.mass;
+            double ay = fy / aff.mass;
+            aff.x = aff.x - ax * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vx * PhysicalObject.seconds;
+            aff.y = aff.y - ay * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vy * PhysicalObject.seconds;
+            aff.vx = aff.vx - ax * PhysicalObject.seconds;
+            aff.vy = aff.vy - ay * PhysicalObject.seconds;
         }
     }
 
     @Override
     protected void doPaintObject(Graphics2D graphics, PhysicalObject po) {
-        po.paintPhysicalObject(graphics, !true);
+        po.paintPhysicalObject(graphics, false);
     }
 
     @Override
     public void mouseWheelMoved(final MouseWheelEvent e) {
-        if (!true) {
-            PhysicalObject.scale = PhysicalObject.scale + PhysicalObject.scale * (Math.min(9, e.getWheelRotation())) / 10 + 0.0001;
-            getGraphics().clearRect(0, 0, getWidth(), getHeight());
-        }
     }
 
 }
--- space/src/main/java/space/Solar.java
+++ space/src/main/java/space/Solar.java
@@ -15,15 +15,13 @@ public class Solar extends Space implements MouseMotionListener {
 
     @Override
     public void mouseDragged(final MouseEvent e) {
-        if (!false) {
-            if (lastDrag == null) {
-                lastDrag = e.getPoint();
-            }
-            PhysicalObject.centrex = PhysicalObject.centrex - ((e.getX() - lastDrag.x) * PhysicalObject.scale);
-            PhysicalObject.centrey = PhysicalObject.centrey - ((e.getY() - lastDrag.y) * PhysicalObject.scale);
+        if (lastDrag == null) {
             lastDrag = e.getPoint();
-            getGraphics().clearRect(0, 0, getWidth(), getHeight());
         }
+        PhysicalObject.centrex = PhysicalObject.centrex - ((e.getX() - lastDrag.x) * PhysicalObject.scale);
+        PhysicalObject.centrey = PhysicalObject.centrey - ((e.getY() - lastDrag.y) * PhysicalObject.scale);
+        lastDrag = e.getPoint();
+        getGraphics().clearRect(0, 0, getWidth(), getHeight());
     }
 
     @Override
@@ -41,34 +39,9 @@ public class Solar extends Space implements MouseMotionListener {
             for (PhysicalObject other : objects) {
                 if (one == other || remove.contains(other))
                     continue;
-                if (!false) {
-                    if (Math.sqrt(Math.pow(one.x - other.x, 2) + Math.pow(one.y - other.y, 2)) < 5e9) {
-                        one.absorb(other);
-                        remove.add(other);
-                    }
-                } else {
-                    double distance = Math.sqrt(Math.pow(one.x - other.x, 2) + Math.pow(one.y - other.y, 2));
-                    double collsionDistance = one.radius + other.radius;
-                    if (distance < collsionDistance) {
-                        one.hitBy(other);
-                    }
-                }
-            }
-            // Wall collision reverses speed in that direction
-            if (false) {
-                if (one.x - one.radius < 0) {
-                    one.vx = -one.vx;
-                }
-                if (one.x + one.radius > 800) {
-                    one.vx = -one.vx;
-                }
-                if (one.y - one.radius < 0) {
-                    one.vy = -one.vy;
-                }
-                if (one.y + one.radius > 800 && !IS_BREAKOUT) {
-                    one.vy = -one.vy;
-                } else if (one.y - one.radius > 800) {
-                    remove.add(one);
+                if (Math.sqrt(Math.pow(one.x - other.x, 2) + Math.pow(one.y - other.y, 2)) < 5e9) {
+                    one.absorb(other);
+                    remove.add(other);
                 }
             }
         }
@@ -77,47 +50,37 @@ public class Solar extends Space implements MouseMotionListener {
 
     @Override
     protected void doStep() {
-        if (!false) {
-            for (PhysicalObject aff : objects) {
-                double fx = 0;
-                double fy = 0;
-                for (PhysicalObject oth : objects) {
-                    if (aff == oth)
-                        continue;
-                    double[] d = new double[]{aff.x - oth.x, aff.y - oth.y};
-                    double r2 = Math.pow(d[0], 2) + Math.pow(d[1], 2);
-                    double f = G * aff.mass * oth.mass / r2;
-                    double sqrtOfR2 = Math.sqrt(r2);
-                    fx += f * d[0] / sqrtOfR2;
-                    fy += f * d[1] / sqrtOfR2;
-                }
-                double ax = fx / aff.mass;
-                double ay = fy / aff.mass;
-                aff.x = aff.x - ax * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vx * PhysicalObject.seconds;
-                aff.y = aff.y - ay * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vy * PhysicalObject.seconds;
-                aff.vx = aff.vx - ax * PhysicalObject.seconds;
-                aff.vy = aff.vy - ay * PhysicalObject.seconds;
+        for (PhysicalObject aff : objects) {
+            double fx = 0;
+            double fy = 0;
+            for (PhysicalObject oth : objects) {
+                if (aff == oth)
+                    continue;
+                double[] d = new double[]{aff.x - oth.x, aff.y - oth.y};
+                double r2 = Math.pow(d[0], 2) + Math.pow(d[1], 2);
+                double f = G * aff.mass * oth.mass / r2;
+                double sqrtOfR2 = Math.sqrt(r2);
+                fx += f * d[0] / sqrtOfR2;
+                fy += f * d[1] / sqrtOfR2;
             }
-        } else {
-            for (PhysicalObject physicalObject : objects) {
-                physicalObject.x = physicalObject.x + physicalObject.vx * PhysicalObject.seconds;
-                physicalObject.y = physicalObject.y + physicalObject.vy * PhysicalObject.seconds;
-            }
-
+            double ax = fx / aff.mass;
+            double ay = fy / aff.mass;
+            aff.x = aff.x - ax * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vx * PhysicalObject.seconds;
+            aff.y = aff.y - ay * Math.pow(PhysicalObject.seconds, 2) / 2 + aff.vy * PhysicalObject.seconds;
+            aff.vx = aff.vx - ax * PhysicalObject.seconds;
+            aff.vy = aff.vy - ay * PhysicalObject.seconds;
         }
     }
 
     @Override
     protected void doPaintObject(Graphics2D graphics, PhysicalObject po) {
-        po.paintPhysicalObject(graphics, !false);
+        po.paintPhysicalObject(graphics, true);
     }
 
     @Override
     public void mouseWheelMoved(final MouseWheelEvent e) {
-        if (!false) {
-            PhysicalObject.scale = PhysicalObject.scale + PhysicalObject.scale * (Math.min(9, e.getWheelRotation())) / 10 + 0.0001;
-            getGraphics().clearRect(0, 0, getWidth(), getHeight());
-        }
+        PhysicalObject.scale = PhysicalObject.scale + PhysicalObject.scale * (Math.min(9, e.getWheelRotation())) / 10 + 0.0001;
+        getGraphics().clearRect(0, 0, getWidth(), getHeight());
     }
 
     public static void main(String[] args) throws InvocationTargetException, InterruptedException {
